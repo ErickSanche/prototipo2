35392b6e6405d91a179ac1e1f5059e0f67e8c983
erick <killofgamers@gmail.com>
2023-03-12 14:31:14 -0600

    login bonito

--- resources/views/Sistema/entrada.blade.php
+++ resources/views/Sistema/entrada.blade.php
@@ -1,77 +1,71 @@
-
-<!-- Define que el documento esta bajo el estandar de HTML 5 -->
 <!doctype html>
-
-<!-- Representa la raíz de un documento HTML o XHTML. Todos los demás elementos deben ser descendientes de este elemento. -->
-<html lang="es">
-    
-    <head>
-        
-        <meta charset="utf-8">
-        
-        <title> Formulario de Acceso </title>    
-        
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <meta name="author" content="Videojuegos & Desarrollo">
-        <meta name="description" content="Ejemplo de formulario de acceso basado en HTML5 y CSS">
-        <meta name="keywords" content="login,formulariode acceso html">
-        
-        <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet"> 
-        
-        <!-- Link hacia el archivo de estilos css -->
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        
-        <style type="text/css">
-            
-        </style>
-        
-        <script type="text/javascript">
-        
-        </script>
-        
-    </head>
-    
-    <body>
-    
-        <div id="contenedor">
-            
-            <div id="contenedorcentrado">
-                <div id="login">
-                    <form action="{{ route('validar') }}" method="post">
-        @csrf
-        <div class="cabeceraS">
-            <div class="titulo">
-            </div>
-            <div class="items">
-                <span class="icono material-symbols-rounded">person</span>
-                <input class="cajas" name="usuario" type="text" placeholder="Usuario">
-            </div>
-            <div class="items">
-                <span class="icono material-symbols-rounded">lock</span>
-                <input class="cajas" name="password" type="text" placeholder="Contraseña">
-            </div>
-            <div class="items">
-                <input class="boton" type="submit" value="Validar">
-            </div>
-        </div>
-    </form>
-                    
-                </div>
-                <div id="derecho">
-                    <div class="titulo">
-                        Bienvenido
-                    </div>
-                    <hr>
-                    <div class="pie-form">
-                        <a href="#">¿Perdiste tu contraseña?</a>
-                        <a href="#">¿No tienes Cuenta? Registrate</a>
-                        <hr>
-                        <a href="#">« Volver</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-    </body>
+<html lang="en">
+<head>
+  <title>Webleb</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
+<body>
+	<div class="section">
+		<div class="container">
+			<div class="row full-height justify-content-center">
+				<div class="col-12 text-center align-self-center py-5">
+					<div class="section pb-5 pt-5 pt-sm-2 text-center">
+						<h6 class="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
+			          	<input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
+			          	<label for="reg-log"></label>
+						<div class="card-3d-wrap mx-auto">
+							<div class="card-3d-wrapper">
+								<div class="card-front">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="mb-4 pb-3">Log In</h4>
+											<div class="form-group">
+												<input type="email" class="form-style" placeholder="Email">
+												<i class="input-icon uil uil-at"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="password" class="form-style" placeholder="Password">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+											<a href="https://www.web-leb.com/code" class="btn mt-4">Login</a>
+                      <p class="mb-0 mt-4 text-center"><a href="https://www.web-leb.com/code" class="link">Forgot your password?</a></p>
+				      					</div>
+			      					</div>
+			      				</div>
+								<div class="card-back">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="mb-3 pb-3">Sign Up</h4>
+											<div class="form-group">
+												<input type="text" class="form-style" placeholder="Full Name">
+												<i class="input-icon uil uil-user"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="tel" class="form-style" placeholder="Phone Number">
+												<i class="input-icon uil uil-phone"></i>
+											</div>	
+                      <div class="form-group mt-2">
+												<input type="email" class="form-style" placeholder="Email">
+												<i class="input-icon uil uil-at"></i>
+											</div>
+											<div class="form-group mt-2">
+												<input type="password" class="form-style" placeholder="Password">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+											<a href="https://www.web-leb.com/code" class="btn mt-4">Register</a>
+				      					</div>
+			      					</div>
+			      				</div>
+			      			</div>
+			      		</div>
+			      	</div>
+		      	</div>
+	      	</div>
+	    </div>
+	</div>
+</body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,6 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
-<body>
     <header>
         <div class="container">
             <div class="row align-items-stretch justify-content-between">
@@ -23,26 +22,28 @@
         </div>
     </header>
 
+<center>
+    <body>
 
+        <div class="container" >
+            <div class="row">
+        <?php
+        for ($i=1; $i <= 9 ; $i++) {
+            echo "<div class='card' style='width: 18rem; style='border-collapse: separate; border-spacing: 10px;''>
+            <img src='../img/$i.jpg' class='card-img-top' alt=''>
+            <div class='card-body'>
+            <h5 class='card-title'>Paquete $i</h5>
+            <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class='card-text'>$5000.</p>
+        </div>
+        </div>";
+        }
+        ?>
+        </div>
+    </body>
+</center>
 
-
-    <div class="container" >
-        <div class="row">
-    <?php
-    for ($i=1; $i <= 9 ; $i++) {
-        echo "<div class='card' style='width: 18rem;'>
-        <img src='../img/$i.jpg' class='card-img-top' alt=''>
-        <div class='card-body'>
-        <h5 class='card-title'>Paquete $i</h5>
-        <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <p class='card-text'>$5000.</p>
-    </div>
-    </div>";
-    }
-    ?>
-    </div>
-    </div>
-</body>
+   
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
