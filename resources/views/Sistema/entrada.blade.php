4e0f72c73da6e94f5d93a31b31834d136014e115
erick <killofgamers@gmail.com>
2023-03-12 14:44:49 -0600

    nda

--- resources/views/Sistema/entrada.blade.php
+++ resources/views/Sistema/entrada.blade.php
@@ -14,7 +14,7 @@
 			<div class="row full-height justify-content-center">
 				<div class="col-12 text-center align-self-center py-5">
 					<div class="section pb-5 pt-5 pt-sm-2 text-center">
-						<h6 class="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
+						<h6 class="mb-0 pb-3"><span>CLIENTES </span><span>ADMINISTRADOR</span></h6>
 			          	<input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
 			          	<label for="reg-log"></label>
 						<div class="card-3d-wrap mx-auto">
@@ -23,6 +23,7 @@
 									<div class="center-wrap">
 										<div class="section text-center">
 											<h4 class="mb-4 pb-3">Log In</h4>
+                                            <form action="{{ route('validar') }}" method="post">
 											<div class="form-group">
 												<input type="email" class="form-style" placeholder="Email">
 												<i class="input-icon uil uil-at"></i>
@@ -33,6 +34,7 @@
 											</div>
 											<a href="https://www.web-leb.com/code" class="btn mt-4">Login</a>
                       <p class="mb-0 mt-4 text-center"><a href="https://www.web-leb.com/code" class="link">Forgot your password?</a></p>
+                                            </form>
 				      					</div>
 			      					</div>
 			      				</div>
