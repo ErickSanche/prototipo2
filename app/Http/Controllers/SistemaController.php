0bff61cded9f6b97c9ec2aa53ae134e70d062fb1
EleazarL <66807291+EleazarL@users.noreply.github.com>
2023-03-13 14:26:49 -0600

    Proyecto FInalizado

--- app/Http/Controllers/SistemaController.php
+++ app/Http/Controllers/SistemaController.php
@@ -14,24 +14,30 @@ class SistemaController extends Controller
 //        dump($solicitud->all());
         $usuario = $solicitud->input('usuario');
         $password = $solicitud->input('password');
-        if($usuario==$password){
+
+        if ($usuario == "cliente" && $password == "cliente") {
             return redirect(route("usuario.paquetes"));
-//            echo "si puede entrar";
-        } else {
+        }
+        else {
+            return view("Sistema.error");
+        }
+
+    }
+    public function validar2(Request $solicitud){
+
+        $usuario = $solicitud->input('usuario');
+        $password = $solicitud->input('password');
+
+        if ($usuario == "gerente" && $password == "gerente") {
+            return redirect(route("paquetes.index"));
+        }
+        if ($usuario == "empleado" && $password == "empleado") {
+            return redirect(route("empleado.eventos"));
+        }  else {
             return view("Sistema.error");
         }
     }
-    public function validar2(Request $solicitud){
-        //        dump($solicitud->all());
-                $usuario = $solicitud->input('usuario');
-                $password = $solicitud->input('password');
-                if($usuario==$password){
-                    return redirect(route("paquetes.index"));
-        //            echo "si puede entrar";
-                } else {
-                    return view("Sistema.error");
-                }
-            }
+
     public function index(){
         return view("usuario.paquetes");
     }
