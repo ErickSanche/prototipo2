75e1a50f9621a2436b9758399d4cb74b817bc5c9
EleazarL <66807291+EleazarL@users.noreply.github.com>
2023-04-24 11:20:19 -0600

    s

--- resources/views/usuario/paquetes.blade.php
+++ resources/views/usuario/paquetes.blade.php
@@ -15,7 +15,7 @@
                 <nav class="navegacion">
                     <ul>
                         <li><a class="navbar-brand" href="#">BIENVENIDO</a></li>
-                        <li><a href="paquetes.create">Agregar Evento</a></li>
+                        <li><a href="/paquetes/create">Agregar Evento</a></li>
                         <li><a href="paquetes.show">Ver Eventos</a></li
                     </ul>
                 </nav>
