d24e55cc25c9cdade29b7caff857a1d5da747e90
ErickSanche <78977117+ErickSanche@users.noreply.github.com>
2023-04-24 13:14:17 -0600

    tablas

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Lista de eventos</title>
-</head>
+<html lang="en">
+    <header>
+        <link rel="stylesheet" href="{{ asset('css/barra.css') }}">
+            <div class="interior">
+                <nav class="navegacion">
+                    <ul>
+                        <li><a href="{{ route('gerente.verusuario') }}">Ver usuarios</a></li>
+                        <li><a href="{{ route('registrar') }}">Agregar usuario</a></li>
+                        <li><a href="{{ route('gerente.verservicios') }}">Ver servicios</a></li>
+                        <li><a href="">Agregar servicios</a></li>
+                        <li><a href="{{ route('salida') }}">Cerrar Sesion</a>
+                    </ul>
+                </nav>
+            </div>
+    </header>
+    <head>
+        <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+       <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
 <body>
 	<h1>Lista de eventos:</h1>
-	<table>
+	<div class="container mt-5">
+                <table id="example" class="table table-striped" style="width:100%">
 		<thead>
 			<tr>
 				<th>Nombre</th>
@@ -23,5 +44,14 @@
 			@endforeach
 		</tbody>
 	</table>
+	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+        <script>
+            $(document).ready(function () {
+    $('#example').DataTable();
+});
+
+        </script>
 </body>
 </html>
