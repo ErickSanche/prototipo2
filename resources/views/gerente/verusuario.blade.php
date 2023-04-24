67489462caaee16ccd4453a8437b4efaab13de36
ErickSanche <78977117+ErickSanche@users.noreply.github.com>
2023-04-24 00:30:49 -0600

    usuarios

--- resources/views/gerente/verusuario.blade.php
+++ resources/views/gerente/verusuario.blade.php
@@ -23,52 +23,70 @@
                         <li><a href="{{ route('gerente.verusuario') }}">Ver usuarios</a></li>
                         <li><a href="{{ route('registrar') }}">Agregar usuario</a></li>
                         <li><a href="{{ route('gerente.verservicios') }}">Ver servicios</a></li>
-                        <li><a href="{{ route('Eventos.store') }}">Agregar servicio</a></li>
+                        <li><a href="{{ route('eventos.store') }}">Agregar servicio</a></li>
                         <li><a href="{{ route('salida') }}">Cerrar Sesion</a>
                     </ul>
                 </nav>
             </div>
     </header>
     <body>
-            <div class="container mt-5">
-                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+    <center>
+    <div class="container mt-5">
+                <table class="table table-striped" style="width:100%">
+
+            <thead>
                 <tr>
                     <th>ID</th>
                     <th>Nombre</th>
-                    <th>Precio</th>
-                    <th>Descripción</th>
-                    <th>Acciones</th>
+                    <th>Usuario</th>
+                    <th>Cargo</th>
                 </tr>
-                    <?php
-                    // Crear una conexión a la base de datos
-                    $db = new PDO('sqlite:C:\Users\erick\Documentos\Framebueno\database\database.sqlite');
+            </thead>
+            <tbody>
+               <tr>
+               <?php
+                        // Crear una conexión a la base de datos
+                        $db = new PDO('sqlite:C:\Users\erick\Documentos\Framebueno\database\database.sqlite');
 
-                    // Ejecutar una consulta SQL
-                
-                    $resultado = $db->query('SELECT * FROM usuarios');
+                        // Ejecutar una consulta SQL
+                    
+                        $resultado = $db->query('SELECT * FROM usuarios');
 
-                    // Mostrar los resultados en una tabla HTML
-                    echo '<table id="example" class="table table-striped" style="width:100%">';
-                    foreach ($resultado as $fila) {
-                        echo '<tr>';
-                        echo '<td>' . $fila['nombre'] . '</td>';
-                        echo '<td>' . $fila['nombre_de_usuario'] . '</td>';
-                        echo '<td>' . $fila['cargo'] . '</td>';
-                        echo '</tr>';
-                    }
-                    echo '</table>';
+                        // Mostrar los resultados en una tabla HTML
+                        echo '<table class="table table-striped" style="width:100%">';
+                        foreach ($resultado as $fila) {
+                            echo '<tr>';
+                            echo '<td>' . $fila['id'] . '</td>';
+                            echo '<td>' . $fila['nombre'] . '</td>';
+                            echo '<td>' . $fila['nombre_de_usuario'] . '</td>';
+                            echo '<td>' . $fila['cargo'] . '</td>';
+                            echo '</tr>';
+                        }
+                        echo '</table>';
 
-                    // Cerrar la conexión a la base de datos
-                    $db = null;
-                    ?>
-                </div>
-            </div>
-        </div>
-        <meta charset="UTF-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-       <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+                        // Cerrar la conexión a la base de datos
+                        $db = null;
+                        ?>
+
+
+               </tr>
+            </tbody>
+
+        </table>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+        <script>
+            $(document).ready(function () {
+    $('#example').DataTable();
+});
+
+        </script>
     </body>
+</body>
+    </center>
+                    |
+    
+          
 </html>
