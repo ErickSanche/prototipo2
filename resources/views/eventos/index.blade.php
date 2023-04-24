f86eda57b026985880638b14c89707681a69913f
EleazarL <66807291+EleazarL@users.noreply.github.com>
2023-04-24 13:22:08 -0600

    a

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -1,30 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Lista de eventos</title>
+    <title>Lista de eventos</title>
 </head>
 <body>
-	<h1>Lista de eventos:</h1>
-	<table>
-		<thead>
-			<tr>
-				<th>Nombre</th>
-				<th>Fecha</th>
-				<th>Lugar</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach ($eventos as $evento)
-			<tr>
-				<td>{{ $evento->nombre }}</td>
-				<td>{{ $evento->fecha }}</td>
-				<td>{{ $evento->lugar }}</td>
-                <td>{{ $evento->lugar }}</td>
-                <td>{{ $evento->lugar }}</td>
-                <td>{{ $evento->lugar }}</td>
-			</tr>
-			@endforeach
-		</tbody>
-	</table>
+    <h1>Lista de eventos:</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Fecha</th>
+                <th>Hora de inicio</th>
+                <th>Hora de fin</th>
+                <th>Número de invitados</th>
+                <th>ID de grupo/paquete</th>
+                <th>Fecha de creación</th>
+                <th>Fecha de actualización</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($eventos as $evento)
+            <tr>
+                <td>{{ $evento->id }}</td>
+                <td>{{ $evento->nombre }}</td>
+                <td>{{ $evento->fecha }}</td>
+                <td>{{ $evento->hora_inicio }}</td>
+                <td>{{ $evento->hora_fin }}</td>
+                <td>{{ $evento->numero_invitados }}</td>
+                <td>{{ $evento->grupopaquete_id }}</td>
+                <td>{{ $evento->created_at }}</td>
+                <td>{{ $evento->updated_at }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
