21ee4f430a3de9c1c8e00b97c1bb9e6d77789fbd
ErickSanche <78977117+ErickSanche@users.noreply.github.com>
2023-04-24 10:07:53 -0600

    Revert "2.0"
    This reverts commit 1d238032a95bb1c3f1b28f5b1c10deef0fd2a813, reversing
    changes made to a6cd3ad9fb637c32d24d44915bf68e61ac8e14f9.

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,24 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuarios;
 use Illuminate\Http\Request;
+
 
 class UsuarioController extends Controller
 {
-    public function review(){
-        return view('usuario.review');
+    public function index(){
+        $usuarios = Usuarios::al();
+        return view('usuarios.index', compact ('eventos'));
     }
 
-    public function registro(){
-        return view('usuario.registro');
+    public function create(){
+        return view('usuario.create');
     }
 
     public function añadir(){
         return view('usuario.añadir');
     }
 
+    public function edit($id)
+    {
+        $evento = usuarios::find($id);
+        return view('usuarios.edit', compact('evento'));
+    }
 
+    public function update(Request $request, $id)
+    {
+        $usuarios = Usuarios::find($id);
+        $usuarios->nombre = $request->input('nombre');
+        $usuarios->nombre_de_usario = $request->input('nombre_de_usario');
+        $usuarios->clave = $request->input('clave');
+        $usuarios->cargo = $request->input('cargo');
+        $usuarios->save();
 
+        return redirect()->route('usuarios.index');
+    }
 
+    public function destroy($id)
+    {
+        $usuarios = Usuarios::find($id);
+        $usuarios->delete();
 
+        return redirect()->route('usuarios.index');
+    }
+
+    public function clear()
+    {
+        Usuarios::truncate();
+
+        return redirect()->route('usuarios.index');
+    }
 }
--- resources/views/gerente/verusuario.blade.php
+++ resources/views/gerente/verusuario.blade.php
@@ -5,53 +5,86 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>DataTables.js</title>
-        <!-- Bootstrap-->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
-        <!-- DataTable -->
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css" />
-        <!-- Font Awesome -->
-        <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-            integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-            crossorigin="anonymous"
-            referrerpolicy="no-referrer"
-        />
+        <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+       <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
         <!-- Custom CSS -->
-        <link rel="stylesheet" href="{{ asset('css/f1.css') }}"> />
     </head>
-    <body>
-        <div class="container my-4">
-            <div class="row">
-                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                    <table id="datatable_users" class="table table-striped">
-                        <caption>
-                            DataTable.js Demo
-                        </caption>
-                        <thead>
-                            <tr>
-                                <th class="centered">#</th>
-                                <th class="centered">Name</th>
-                                <th class="centered">Email</th>
-                                <th class="centered">City</th>
-                                <th class="centered">Company</th>
-                                <th class="centered">Status</th>
-                                <th class="centered">Options</th>
-                            </tr>
-                        </thead>
-                        <tbody id="tableBody_users"></tbody>
-                    </table>
-                </div>
+    <header>
+        <link rel="stylesheet" href="{{ asset('css/barra.css') }}">
+            <div class="interior">
+                <nav class="navegacion">
+                    <ul>
+                        <li><a href="{{ route('gerente.verusuario') }}">Ver usuarios</a></li>
+                        <li><a href="{{ route('registrar') }}">Agregar usuario</a></li>
+                        <li><a href="{{ route('gerente.verservicios') }}">Ver servicios</a></li>
+                        <li><a href="{{ route('eventos.store') }}">Agregar servicio</a></li>
+                        <li><a href="{{ route('salida') }}">Cerrar Sesion</a>
+                    </ul>
+                </nav>
             </div>
-        </div>
-        <!-- Bootstrap-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-        <!-- jQuery -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <!-- DataTable -->
-        <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
-        <!-- Custom JS -->
-        <script src="{{ asset('js/index.js') }}"></script>
+    </header>
+    <body>
+    <link rel="stylesheet" href="{{ asset('css/f1.css') }}">    
+
+    <center>
+    <div class="container mt-5">
+    <table id="example" class="table table-striped" style="width:100%">
+
+<thead>
+    <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>nombre de usuario</th>
+        <th>Cargo</th>
+        <th>Acciones</th>
+    </tr>
+</thead>
+<tbody>
+    @foreach ($usuarios as $usuario)
+    <tr>
+        <td>{{ $usuario->id }}</td>
+        <td>{{ $usuario->nombre }}</td>
+        <td>{{ $usuario->nombre_de_usario }}</td>
+        <td>{{ $usuario->cargo }}</td>
+        <td >
+        <form action="{{ route('usuarios.destroy', $usuario->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn-delete">
+            <i class="fa fa-trash"></i> Eliminar
+        </button>
+        </form>
+
+        <button type="button" class="btn-edit">
+        <a href="{{ route('usuarios.edit', $usuario->id) }}">
+            <i class="fa fa-pencil"></i> Editar
+        </a>
+        </button>
+        </td>
+    </tr>
+    @endforeach
+</tbody>
+
+</table>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+        <script>
+            $(document).ready(function () {
+    $('#example').DataTable();
+});
+
+        </script>
     </body>
+</body>
+    </center>
+                    |
+    
+          
 </html>
