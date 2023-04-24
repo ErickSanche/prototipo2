3d204805c1a68050e62851fb0af6880f95686757
EleazarL <66807291+EleazarL@users.noreply.github.com>
2023-04-24 14:27:48 -0600

    s

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,6 @@ Route::get('/', [PaqueteController::class, 'welcome'])->name('welcome');
 Route::get('/eventos/create', [EventoController::class, 'create'])->name('eventos.create');
 Route::get('/eventos/mostrar', [EventoController::class, 'mostrareventos'])->name('eventos.mostrar');
 Route::post('/eventos', [EventoController::class, 'store'])->name('eventos.store');
-Route::get('/eventos/{evento}', [EventoController::class, 'show'])->name('eventos.show');
 Route::get('/eventos/{evento}/edit', [EventoController::class, 'edit'])->name('eventos.edit');
 Route::put('/eventos/{evento}', [EventoController::class, 'update'])->name('eventos.update');
-Route::delete('/eventos/{evento}', [EventoController::class, 'destroy'])->name('eventos.destroy');
+Route::get('/eventos/{evento}', [EventoController::class, 'destroy'])->name('eventos.destroy');
