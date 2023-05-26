600d3b4fb37297fd7b45c5a8cea99c6eb14bdcbe
ErickSanche <78977117+ErickSanche@users.noreply.github.com>
2023-05-26 14:48:27 -0600

    mas vistas

--- resources/views/eventos/create.blade.php
+++ resources/views/eventos/create.blade.php
@@ -1,78 +1,116 @@
 <!DOCTYPE html>
-<html>
+<html lang="es-ES">
+
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Esta descripción es la que aparece en los buscadores debajo de la URL" />
+	<link href="./styles/practica7.css" rel="stylesheet" type="text/css" />
+	<link href="./styles/formulario.css" rel="stylesheet" type="text/css" />
+	<link href="./styles/resets.css" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,500,600,700,800&display=swap" rel="stylesheet">
 	<title>Crear evento</title>
+    <link rel="stylesheet" href="{{ asset('css/barra.css') }}">
     <link rel="stylesheet" href="{{ asset('css/s2.css') }}">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-+y/hcN0I7wsNfW1QH0b3qnaF0gB/KxTyK1QXto3OkPUgZi6ky1hL/nRjAvEYdBbgU5ECVRU5r6U5Xafy1ld6aw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
 </head>
+<style>
+		.column {
+			width: 50%;
+			float: left;
+			box-sizing: border-box;
+			padding: 0 10px;
+		}
+
+		.clearfix::after {
+			content: "";
+			display: table;
+			clear: both;
+		}
+
+		@media (max-width: 600px) {
+			.column {
+				width: 100%;
+			}
+		}
+	</style>
+
 <body>
-<div class="signup-container">
-  <div class="left-container">
-    <h1>
-      <i class="fas fa-paw"></i>
-      Create Evento
-    </h1>
-    <div class="puppy">
-      <img src=https://ingenieriademenu.com/wp-content/uploads/2021/06/Como-ser-un-buen-organizador-de-eventos.jpg">
+
+<header>
+    <div class="interior">
+      <nav class="navegacion">
+        <ul>
+          <li><a href="{{ route('eventos.index') }}">Ver Eventos</a></li>
+          <li><a href="{{ route('eventos.create') }}">Agregar Evento</a></li>
+        </ul>
+      </nav>
     </div>
-  </div>
-  <div class="right-container">
-    <header>
-      <div class="set">
-        <form method="POST" action="{{ route('eventos.store') }}" enctype="multipart/form-data">
-            @csrf
+  </header>
 
-            <div class="pets-name">
-                <label for="nombre">Nombre:</label>
-                <input type="text" name="nombre" id="nombre" class="form-control" required>
-            </div>
+	<main>
+		<h1>Create Evento</h1>
+        <div class="clearfix">
+			<div class="column">
+  		<div class="field">
+			<form method="POST" action="{{ route('eventos.store') }}" enctype="multipart/form-data">
+				@csrf
 
-            <div class="form-group">
-                <label for="fecha">Fecha:</label>
-                <input type="date" name="fecha" id="fecha" class="form-control" required>
-            </div>
+				<div class="field">
+					<label for="nombre">Nombre:</label>
+					<input type="text" name="nombre" id="nombre" class="form-control" required>
+				</div>
 
-            <div class="form-group">
-                <label for="hora_inicio">Hora de inicio:</label>
-                <input type="time" name="hora_inicio" id="hora_inicio" class="form-control" required>
-            </div>
+				<div class="field">
+					<label for="fecha">Fecha:</label>
+					<input type="date" name="fecha" id="fecha" class="form-control" required>
+				</div>
 
-            <div class="form-group">
-                <label for="hora_fin">Hora de fin:</label>
-                <input type="time" name="hora_fin" id="hora_fin" class="form-control" required>
-            </div>
+				 <div class="field">
+					<label for="hora_inicio">Hora de inicio:</label>
+					<input type="time" name="hora_inicio" id="hora_inicio" class="form-control" required>
+				</div>
 
-            <div class="form-group">
-                <label for="numero_invitados">Número de invitados:</label>
-                <input type="number" name="numero_invitados" id="numero_invitados" class="form-control" required>
-            </div>
+				<div class="field">
+					<label for="hora_fin">Hora de fin:</label>
+					<input type="time" name="hora_fin" id="hora_fin" class="form-control" required>
+				</div>
 
-            <div class="form-group">
-                <label for="grupopaquete_id">Paquete:</label>
-                <select name="grupopaquete_id" id="grupopaquete_id" class="form-control" required>
-                    <option value="">Seleccione un paquete</option>
-                    @foreach($grupos_paquetes as $paquete)
-                        <option value="{{ $paquete->id }}">{{ $paquete->nombre }} - ${{ $paquete->precio }}</option>
-                    @endforeach
-                </select>
-            </div>
+				<div class="textBox">
+					<label for="numero_invitados">Número de invitados:</label>
+					<input type="number" name="numero_invitados" id="numero_invitados" class="form-control" required>
+				</div>
 
-            <div class="form-group">
-                <label for="servicios">Servicios:</label>
-                <select name="servicios[]" id="servicios" class="form-control" multiple required>
-                    @foreach($servicios as $servicio)
-                        <option value="{{ $servicio->id }}">{{ $servicio->nombre }} - ${{ $servicio->precio }}</option>
-                    @endforeach
-                </select>
-            </div>
+				<div class="field">
+					<label for="grupopaquete_id">Paquete:</label>
+					<select name="grupopaquete_id" id="grupopaquete_id" class="form-control" required>
+						<option value="">Seleccione un paquete</option>
+						@foreach($grupos_paquetes as $paquete)
+							<option value="{{ $paquete->id }}">{{ $paquete->nombre }} - ${{ $paquete->precio }}</option>
+						@endforeach
+					</select>
+				</div>
 
-            <div class="form-group">
-                <label for="imagenes">Imágen</label>
-                <input type="file" name="imagen[]" id="imagen" multiple required>
-            </div>
+				<div class="field">
+					<label for="servicios">Servicios:</label>
+					<select name="servicios[]" id="servicios" class="form-control" multiple required>
+						@foreach($servicios as $servicio)
+							<option value="{{ $servicio->id }}">{{ $servicio->nombre }} - ${{ $servicio->precio }}</option>
+						@endforeach
+					</select>
+				</div>
 
-            <button type="submit" class="btn btn-primary">Crear Evento</button>
-        </form>
-    </div>
+				<div class="field">
+					<label for="imagenes">Imágen</label>
+					<input type="file" name="imagen[]" id="imagen" multiple required>
+				</div>
+
+				<button type="submit" class="btn btn-primary">Crear Evento</button>
+			</form>
+		</div>
+        </div>
+        </div>
+	</main>
+</body>
 
 </html>
--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -1,105 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
-    <header>
-        <link rel="stylesheet" href="{{ asset('css/barra.css') }}">
-        <div class="interior">
-            <nav class="navegacion">
-            </nav>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('css/card.css') }}">
+</head>
+
+<body>
+    <div class="card">
+        @foreach ($eventos as $evento)
+        <div class="card_landing" style="--i:url(img.jpg)">
+            <h6>eventos</h6>
+            @if ($evento->imagen)
+            @foreach (explode(',', $evento->imagen) as $imagen)
+            <img src="{{ asset("app/public/$imagen") }} width="250" height="150" alt="">
+            @endforeach
+            @endif
         </div>
-    </header>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="{{ asset('css/eventos.css') }}">
-    </head>
-
-    <body>
-        <h1>Lista de eventos:</h1>
-        <div class="container mt-5">
-            <body>
-                <h1>Lista de eventos:</h1>
-                <div class="container mt-5">
-                    <table id="example" class="table table-striped" style="width:100%">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Nombre</th>
-                                <th>Fecha</th>
-                                <th>Hora de inicio</th>
-                                <th>Hora de fin</th>
-                                <th>Número de invitados</th>
-                                <th>Imagenes</th>
-                                <th>Servicios</th>
-                                <th>Precio Total</th>
-                                <th>Grupo/Paquete</th>
-                                <th>Estado</th>
-                                <th>Acciones</th> <!-- Nueva columna para las acciones -->
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($eventos as $evento)
-                            <tr>
-                                <td>{{ $evento->id }}</td>
-                                <td>{{ $evento->nombre }}</td>
-                                <td>{{ $evento->fecha }}</td>
-                                <td>{{ $evento->hora_inicio }}</td>
-                                <td>{{ $evento->hora_fin }}</td>
-                                <td>{{ $evento->numero_invitados }}</td>
-                                <td>
-                                    @if ($evento->imagen)
-                                        @foreach (explode(',', $evento->imagen) as $imagen)
-                                            <img src="{{ asset("app/public/$imagen") }}" width="250" height="150" alt="">
-                                        @endforeach
-                                    @endif
-                                </td>
-
-
-                                <td>
-                                    @foreach ($evento->servicios as $servicio)
-                                        {{ $servicio->nombre }}<br>
-                                    @endforeach
-                                </td>
-                                <td>{{ $evento->precio_total }}</td>
-                                <td>{{ $evento->grupopaquete_id}}</td>
-                                <td>{{ $evento->estado }}</td>
-                                <td>
-                                    <!-- Enlace para editar el evento -->
-                                    <a href="{{ route('eventos.edit', $evento->id) }}">Editar</a>
-
-                                    <!-- Formulario para eliminar el evento -->
-                                    <form method="POST" action="{{ route('eventos.destroy', $evento->id) }}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit">Eliminar</button>
-                                    </form>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+        <div class="card_info">
+            <div class="head">
+                <p class="title">evento</p>
+                <div class="description">
+                    <div class="item">
+                        <p>{{ $evento->id }}</p>
+                    </div>
+                    <div class="item">
+                    </div>
                 </div>
-                <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-                <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-                <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
-                <script>
-                    $(document).ready(function () {
-                        $('#example').DataTable();
-                    });
-                </script>
-            </body>
+            </div>
 
+            <div class="content">
+                <p class="title">datos del evento:</p>
+                <ul class="list">
+                    <li>{{ $evento->nombre }}</li>
+                    <li>{{ $evento->fecha }}</li>
+                    <li>{{ $evento->hora_inicio }}</li>
+                    <li>{{ $evento->hora_fin }}</li>
+                    <li>{{ $evento->numero_invitados }}</li>
+                    <li>
+                        @foreach ($evento->servicios as $servicio)
+                        {{ $servicio->nombre }}<br>
+                        @endforeach
+                    </li>
+                    <li>{{ $evento->precio_total }}</li>
+                    <li>{{ $evento->grupopaquete_id }}</li>
+                    <li>{{ $evento->estado }}</li>
+                    <li>
+                        <!-- Enlace para editar el evento -->
+                        <a href="{{ route('eventos.edit', $evento->id) }}">Editar</a>
+
+                        <!-- Formulario para eliminar el evento -->
+                        <form method="POST" action="{{ route('eventos.destroy', $evento->id) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Eliminar</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
         </div>
-	    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
-        <script>
-            $(document).ready(function () {
-                $('#example').DataTable();
-            });
-        </script>
-    </body>
+        @endforeach
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        });
+    </script>
+</body>
+
 </html>
