b52580d00ed52eea027d1969bd00e9f50b484b8a
ErickSanche <78977117+ErickSanche@users.noreply.github.com>
2023-05-29 14:03:13 -0600

    mas vitas

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -13,6 +13,7 @@
   <link rel="stylesheet" href="{{ asset('css/card.css') }}">
   <link rel="stylesheet" href="{{ asset('css/barra.css') }}">
   <link rel="stylesheet" href="{{ asset('css/slider.css') }}">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
 </head>
 
 <body>
@@ -31,22 +32,26 @@
 
   <div class="img-gallery">
     @foreach ($eventos as $evento)
-      @if ($evento->imagen)
-        @foreach (explode(',', $evento->imagen) as $imagen)
-          <img src="{{ asset("app/public/$imagen") }}" onclick="openFulImg(this.src)" width="30" height="70%" alt="">
-        @endforeach
-      @endif
-  
-    <div class="card_info">
-      <div class="head">
-        <p class="title">Datos del evento</p>
-        <div class="description">
-          <div class="item">
-            <i>{{ $evento->id }}</i>
-            <i>{{ $evento->nombre }}</i>
+      <div class="card_info">
+        @if ($evento->imagen)
+          <div class="slider">
+            @foreach (explode(',', $evento->imagen) as $imagen)
+              <div>
+                <img src="{{ asset("app/public/$imagen") }}" onclick="openFulImg(this.src)" alt="">
+              </div>
+            @endforeach
+          </div>
+        @endif
+
+        <div class="head">
+          <p class="title">Datos del evento</p>
+          <div class="description">
+            <div class="item">
+              <i>{{ $evento->id }}</i>
+              <i>{{ $evento->nombre }}</i>
+            </div>
           </div>
         </div>
-      </div>
 
         <ul class="list">
           <li>Fecha: {{ $evento->fecha }}</li>
@@ -55,7 +60,7 @@
           <li>Invitados: {{ $evento->numero_invitados }}</li>
           <li>
             @foreach ($evento->servicios as $servicio)
-            {{ $servicio->nombre }}<br>
+              {{ $servicio->nombre }}<br>
             @endforeach
           </li>
           <li>Precio: {{ $evento->precio_total }}</li>
@@ -63,44 +68,43 @@
           <li>Estado: {{ $evento->estado }}</li>
           <p>
             @can('update', $evento)
-            <a href="{{ route('eventos.edit', $evento->id) }}" class="btn btn-primary">Editar</a>
+              <a href="{{ route('eventos.edit', $evento->id) }}" class="btn btn-primary">Editar</a>
             @endcan
 
             @can('delete', $evento)
-            @if ($evento->estado !== 'Validando')
-            <form method="POST" action="{{ route('eventos.destroy', $evento->id) }}" class="d-inline">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-danger">Eliminar</button>
-            </form>
-            @endif
+              @if ($evento->estado !== 'Validando')
+                <form method="POST" action="{{ route('eventos.destroy', $evento->id) }}" class="d-inline">
+                  @csrf
+                  @method('DELETE')
+                  <button type="submit" class="btn btn-danger">Eliminar</button>
+                </form>
+              @endif
             @endcan
 
             @if (auth()->user()->tipo === 'administrador' || auth()->user()->tipo === 'empleado')
-            <a href="{{ route('eventos.vistaAbonar', $evento->id) }}" class="btn btn-primary">Abonar</a>
+              <a href="{{ route('eventos.vistaAbonar', $evento->id) }}" class="btn btn-primary">Abonar</a>
             @endif
-  </p>
+          </p>
         </ul>
       </div>
-    </div>
+    @endforeach
   </div>
-
-  </div>
-  </div>
-  @endforeach
 
   <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
   <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
   <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick.min.js"></script>
   <script>
     $(document).ready(function() {
+      $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+
       $('#example').DataTable();
     });
   </script>
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-  <script src="{{ asset('js/slider.js') }}"></script>
 </body>
 
 </html>
