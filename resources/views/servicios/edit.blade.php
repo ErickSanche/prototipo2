4fd4d559c8946f0ace27003f87580604e9be824a
ErickSanche <78977117+ErickSanche@users.noreply.github.com>
2023-05-29 15:47:57 -0600

    a

--- resources/views/servicios/edit.blade.php
+++ resources/views/servicios/edit.blade.php
@@ -1,1 +1,70 @@
-hola xd
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Editar Servicio') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('servicios.update', $servicio->id) }}">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="form-group row">
+                            <label for="nombre" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre', $servicio->nombre) }}" required autocomplete="nombre" autofocus>
+
+                                @error('nombre')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="descripcion" class="col-md-4 col-form-label text-md-right">{{ __('Descripción') }}</label>
+
+                            <div class="col-md-6">
+                                <textarea id="descripcion" class="form-control @error('descripcion') is-invalid @enderror" name="descripcion" required>{{ old('descripcion', $servicio->descripcion) }}</textarea>
+
+                                @error('descripcion')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="precio" class="col-md-4 col-form-label text-md-right">{{ __('Precio') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="precio" type="number" step="0.01" class="form-control @error('precio') is-invalid @enderror" name="precio" value="{{ old('precio', $servicio->precio) }}" required>
+
+                                @error('precio')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Guardar') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
